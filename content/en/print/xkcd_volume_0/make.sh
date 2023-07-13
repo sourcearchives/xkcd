132cfacab4b3f6506fa506822d742cd73df2c041
gabldotink <gabl@gabl.ink>
2023-07-13 10:40:19 -0600

    edit volume 0 files to reflect file renames and changes
    Authored-by: gabldotink <gabl@gabl.ink>
    Signed-off-by: gabldotink <gabl@gabl.ink>
    SPDX-License-Identifier: CC0-1.0

--- content/en/print/xkcd_volume_0/make.sh
+++ content/en/print/xkcd_volume_0/make.sh
@@ -49,131 +49,131 @@ usage: %s cbz
 Build requirements are below:
 - cbz requires
   - the zip application in your $PATH
-  - the 118 PNG files in the ./pages/png/ directory
+  - the 118 PNG files in the ./page/png/ directory
   - (preferably) the file ./cbz/mimetype
   - (preferably) ./metadata/ComicInfo.xml and ./metadata/Cover.png .\n' "$script"
 }
 
 png_pages () {
-  p001='./pages/png/001.png' # Yes. I am
-  p002='./pages/png/002.png' # hardcoding
-  p003='./pages/png/003.png' # this manually,
-  p004='./pages/png/004.png' # and it is
-  p005='./pages/png/005.png' # on purpose.
-  p006='./pages/png/006.png' # No wildcards.
-  p007='./pages/png/007.png' # No seqs.
-  p008='./pages/png/008.png' # No friends.
-  p009='./pages/png/009.png' # I am simply
-  p010='./pages/png/010.png' # too efficient
-  p011='./pages/png/011.png' # for you to
-  p012='./pages/png/012.png' # truly get it.
-  p013='./pages/png/013.png' # I have
-  p014='./pages/png/014.png' # transcended
-  p015='./pages/png/015.png' # simplicity.
-  p016='./pages/png/016.png' # People will
-  p017='./pages/png/017.png' # call this bad
-  p018='./pages/png/018.png' # because they
-  p019='./pages/png/019.png' # are jealous of
-  p020='./pages/png/020.png' # my power.
-  p021='./pages/png/021.png' # Do you
-  p022='./pages/png/022.png' # understand?
-  p023='./pages/png/023.png' # I have broken
-  p024='./pages/png/024.png' # free from this
-  p025='./pages/png/025.png' # mortal coil.
-  p026='./pages/png/026.png' # I will enter
-  p027='./pages/png/027.png' # your dreams.
-  p028='./pages/png/028.png' # Enter your
-  p029='./pages/png/029.png' # nightmares.
-  p030='./pages/png/030.png' # The variables
-  p031='./pages/png/031.png' # will stick in
-  p032='./pages/png/032.png' # your mind,
-  p033='./pages/png/033.png' # like a pancake
-  p034='./pages/png/034.png' # sticks to the
-  p035='./pages/png/035.png' # kitchen
-  p036='./pages/png/036.png' # ceiling.
-  p037='./pages/png/037.png' # Like tree sap
-  p038='./pages/png/038.png' # sticks to
-  p039='./pages/png/039.png' # your hands.
-  p040='./pages/png/040.png' # Like a stick.
-  p041='./pages/png/041.png' # A tree stick.
-  p042='./pages/png/042.png' # Made of wood.
-  p043='./pages/png/043.png' # Like your
-  p044='./pages/png/044.png' # fatuous brain.
-  p045='./pages/png/045.png' # Made of wood.
-  p046='./pages/png/046.png' # That is why
-  p047='./pages/png/047.png' # you are so
-  p048='./pages/png/048.png' # dumb. So dumb.
-  p049='./pages/png/049.png' # Imbecilic.
-  p050='./pages/png/050.png' # Your brain is
-  p051='./pages/png/051.png' # a tiny cone.
-  p052='./pages/png/052.png' # A pine cone.
-  p053='./pages/png/053.png' # A fir cone.
-  p054='./pages/png/054.png' # A spruce cone.
-  p055='./pages/png/055.png' # A waffle cone.
-  p056='./pages/png/056.png' # Your brain is
-  p057='./pages/png/057.png' # a waffle,
-  p058='./pages/png/058.png' # which is like
-  p059='./pages/png/059.png' # a pancake,
-  p060='./pages/png/060.png' # which is what
-  p061='./pages/png/061.png' # sticks on the
-  p062='./pages/png/062.png' # ceiling, like
-  p063='./pages/png/063.png' # my programming
-  p064='./pages/png/064.png' # practices in
-  p065='./pages/png/065.png' # your head.
-  p066='./pages/png/066.png' # You will never
-  p067='./pages/png/067.png' # escape me.
-  p068='./pages/png/068.png' # I am in your
-  p069='./pages/png/069.png' # house. Whether
-  p070='./pages/png/070.png' # literally or
-  p071='./pages/png/071.png' # metaphorically
-  p072='./pages/png/072.png' # you will never
-  p073='./pages/png/073.png' # know. To fight
-  p074='./pages/png/074.png' # is to die; to
-  p075='./pages/png/075.png' # run is to be
-  p076='./pages/png/076.png' # a coward. A
-  p077='./pages/png/077.png' # stupid coward,
-  p078='./pages/png/078.png' # in your case.
-  p079='./pages/png/079.png' # Because you
-  p080='./pages/png/080.png' # are stupid.
-  p081='./pages/png/081.png' # I used to be
-  p082='./pages/png/082.png' # stupid too.
-  p083='./pages/png/083.png' # I was blind.
-  p084='./pages/png/084.png' # I was a sheep.
-  p085='./pages/png/085.png' # “No, you
-  p086='./pages/png/086.png' # shouldn’t
-  p087='./pages/png/087.png' # write a
-  p088='./pages/png/088.png' # 200-line shell
-  p089='./pages/png/089.png' # script with
-  p090='./pages/png/090.png' # 118 explicit
-  p091='./pages/png/091.png' # variable
-  p092='./pages/png/092.png' # assignments!
-  p093='./pages/png/093.png' # That’s just
-  p094='./pages/png/094.png' # madness!”
-  p095='./pages/png/095.png' # And you know
-  p096='./pages/png/096.png' # what? I
-  p097='./pages/png/097.png' # believed them.
-  p098='./pages/png/098.png' # I, too, was
-  p099='./pages/png/099.png' # brainwashed by
-  p100='./pages/png/100.png' # the masses.
-  p101='./pages/png/101.png' # But I realized
-  p102='./pages/png/102.png' # something.
-  p103='./pages/png/103.png' # Madness is
-  p104='./pages/png/104.png' # not madness.
-  p105='./pages/png/105.png' # Madness is
-  p106='./pages/png/106.png' # genius.
-  p107='./pages/png/107.png' # It always is.
-  p108='./pages/png/108.png' # A madman is
-  p109='./pages/png/109.png' # greater than
-  p110='./pages/png/110.png' # all the sane
-  p111='./pages/png/111.png' # ones. And
-  p112='./pages/png/112.png' # everyone knows
-  p113='./pages/png/113.png' # it. You know
-  p114='./pages/png/114.png' # it. I know it.
-  p115='./pages/png/115.png' # I truly am a
-  p116='./pages/png/116.png' # lunatic.
-  p117='./pages/png/117.png' # Because only
-  p118='./pages/png/118.png' # I see my genius.
-        # (okay, but seriously, TODO: refactor)
+  p001='./page/png/001.png' # Yes. I am
+  p002='./page/png/002.png' # hardcoding
+  p003='./page/png/003.png' # this manually,
+  p004='./page/png/004.png' # and it is
+  p005='./page/png/005.png' # on purpose.
+  p006='./page/png/006.png' # No wildcards.
+  p007='./page/png/007.png' # No seqs.
+  p008='./page/png/008.png' # No friends.
+  p009='./page/png/009.png' # I am simply
+  p010='./page/png/010.png' # too efficient
+  p011='./page/png/011.png' # for you to
+  p012='./page/png/012.png' # truly get it.
+  p013='./page/png/013.png' # I have
+  p014='./page/png/014.png' # transcended
+  p015='./page/png/015.png' # simplicity.
+  p016='./page/png/016.png' # People will
+  p017='./page/png/017.png' # call this bad
+  p018='./page/png/018.png' # because they
+  p019='./page/png/019.png' # are jealous of
+  p020='./page/png/020.png' # my power.
+  p021='./page/png/021.png' # Do you
+  p022='./page/png/022.png' # understand?
+  p023='./page/png/023.png' # I have broken
+  p024='./page/png/024.png' # free from this
+  p025='./page/png/025.png' # mortal coil.
+  p026='./page/png/026.png' # I will enter
+  p027='./page/png/027.png' # your dreams.
+  p028='./page/png/028.png' # Enter your
+  p029='./page/png/029.png' # nightmares.
+  p030='./page/png/030.png' # The variables
+  p031='./page/png/031.png' # will stick in
+  p032='./page/png/032.png' # your mind,
+  p033='./page/png/033.png' # like a pancake
+  p034='./page/png/034.png' # sticks to the
+  p035='./page/png/035.png' # kitchen
+  p036='./page/png/036.png' # ceiling.
+  p037='./page/png/037.png' # Like tree sap
+  p038='./page/png/038.png' # sticks to
+  p039='./page/png/039.png' # your hands.
+  p040='./page/png/040.png' # Like a stick.
+  p041='./page/png/041.png' # A tree stick.
+  p042='./page/png/042.png' # Made of wood.
+  p043='./page/png/043.png' # Like your
+  p044='./page/png/044.png' # fatuous brain.
+  p045='./page/png/045.png' # Made of wood.
+  p046='./page/png/046.png' # That is why
+  p047='./page/png/047.png' # you are so
+  p048='./page/png/048.png' # dumb. So dumb.
+  p049='./page/png/049.png' # Imbecilic.
+  p050='./page/png/050.png' # Your brain is
+  p051='./page/png/051.png' # a tiny cone.
+  p052='./page/png/052.png' # A pine cone.
+  p053='./page/png/053.png' # A fir cone.
+  p054='./page/png/054.png' # A spruce cone.
+  p055='./page/png/055.png' # A waffle cone.
+  p056='./page/png/056.png' # Your brain is
+  p057='./page/png/057.png' # a waffle,
+  p058='./page/png/058.png' # which is like
+  p059='./page/png/059.png' # a pancake,
+  p060='./page/png/060.png' # which is what
+  p061='./page/png/061.png' # sticks on the
+  p062='./page/png/062.png' # ceiling, like
+  p063='./page/png/063.png' # my programming
+  p064='./page/png/064.png' # practices in
+  p065='./page/png/065.png' # your head.
+  p066='./page/png/066.png' # You will never
+  p067='./page/png/067.png' # escape me.
+  p068='./page/png/068.png' # I am in your
+  p069='./page/png/069.png' # house. Whether
+  p070='./page/png/070.png' # literally or
+  p071='./page/png/071.png' # metaphorically
+  p072='./page/png/072.png' # you will never
+  p073='./page/png/073.png' # know. To fight
+  p074='./page/png/074.png' # is to die; to
+  p075='./page/png/075.png' # run is to be
+  p076='./page/png/076.png' # a coward. A
+  p077='./page/png/077.png' # stupid coward,
+  p078='./page/png/078.png' # in your case.
+  p079='./page/png/079.png' # Because you
+  p080='./page/png/080.png' # are stupid.
+  p081='./page/png/081.png' # I used to be
+  p082='./page/png/082.png' # stupid too.
+  p083='./page/png/083.png' # I was blind.
+  p084='./page/png/084.png' # I was a sheep.
+  p085='./page/png/085.png' # “No, you
+  p086='./page/png/086.png' # shouldn’t
+  p087='./page/png/087.png' # write a
+  p088='./page/png/088.png' # 200-line shell
+  p089='./page/png/089.png' # script with
+  p090='./page/png/090.png' # 118 explicit
+  p091='./page/png/091.png' # variable
+  p092='./page/png/092.png' # assignments!
+  p093='./page/png/093.png' # That’s just
+  p094='./page/png/094.png' # madness!”
+  p095='./page/png/095.png' # And you know
+  p096='./page/png/096.png' # what? I
+  p097='./page/png/097.png' # believed them.
+  p098='./page/png/098.png' # I, too, was
+  p099='./page/png/099.png' # brainwashed by
+  p100='./page/png/100.png' # the masses.
+  p101='./page/png/101.png' # But I realized
+  p102='./page/png/102.png' # something.
+  p103='./page/png/103.png' # Madness is
+  p104='./page/png/104.png' # not madness.
+  p105='./page/png/105.png' # Madness is
+  p106='./page/png/106.png' # genius.
+  p107='./page/png/107.png' # It always is.
+  p108='./page/png/108.png' # A madman is
+  p109='./page/png/109.png' # greater than
+  p110='./page/png/110.png' # all the sane
+  p111='./page/png/111.png' # ones. And
+  p112='./page/png/112.png' # everyone knows
+  p113='./page/png/113.png' # it. You know
+  p114='./page/png/114.png' # it. I know it.
+  p115='./page/png/115.png' # I truly am a
+  p116='./page/png/116.png' # lunatic.
+  p117='./page/png/117.png' # Because only
+  p118='./page/png/118.png' # I see my genius.
+       # (okay, but seriously, TODO: refactor)
   if [ -s "$p001" ] && [ -s "$p002" ] && \
      [ -s "$p003" ] && [ -s "$p004" ] && \
      [ -s "$p005" ] && [ -s "$p006" ] && \
@@ -259,7 +259,7 @@ png_pages () {
 
 make_cbz () {
   if command -v zip >/dev/null 2>&1; then
-    if [ -e './pages/png' ]; then
+    if [ -e './page/png' ]; then
       png_pages
       if [ "$png_pages_success" = 'yes' ]; then
         if [ ! -s './cbz/mimetype' ]; then mimetype='no'; fi
@@ -274,7 +274,7 @@ make_cbz () {
         else printf 'Something went wrong. cbz was not made.\n'; quit; exit 1
         fi
       fi
-    else printf 'The directory ./pages/png/ does not exist. cbz cannot be made.\n'; exit 1
+    else printf 'The directory ./page/png/ does not exist. cbz cannot be made.\n'; exit 1
     fi
   else printf 'zip is not in your $PATH. cbz cannot be made.\n'; exit 1
 fi ;}
