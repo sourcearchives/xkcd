cebc1258fecd38b3ca4f2775d2d2ed5885897e14
Gabriel “gabldotink” <gabl@gabl.ink>
2024-02-14 14:44:30 -0700

    create github_update script
    coming soon:
    • script refactoring
    • github json updates

---
+++ script/github_update.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# run this script from the repository root: ./script/github_update.sh
+#                                  ( or: sh ./script/github_update.sh )
+LC_ALL=C
+readonly LC_ALL
+export LC_ALL
+readonly POSIXLY_CORRECT
+export POSIXLY_CORRECT
+
+if [ "$#" !=  1 ]||
+   [ "$1"  = '' ];then
+  printf \
+'usage: ./script/github_update.sh owner/repo
+Please run this script from the repository root so that it doesn’t get confused about file paths.
+
+This script updates the GitHub API JSON for the repository name you provide.
+It does not operate on bundle files or create new files or directories.\n'
+  set -x
+  exit 1
+fi
+
+set -x
+
+c="./content/en/thirdparty/git/github.com/$1"
+readonly c
+export c
+
+if [ -f "$c/info.json" ];then
+  curl "https://api.github.com/repos/$1" --output "$c/info.json"
+else
+  printf '[error] Couldn’t find file: %s\n' "$c/info.json"
+  printf '[error] The script has stopped due to the above error.
+Operated on no files.\n'
+  exit 1
+fi
+
+printf 'Done. Operated on files below.
+%s\n' "$c/info.json"
+exit 0
