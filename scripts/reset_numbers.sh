ca20abb3ed406c5d5c998794a1acab6821fb86a4
Gabriel “gabldotink” <gabl@gabl.ink>
2024-03-31 17:11:26 -0600

    create reset_numbers.sh

---
+++ scripts/reset_numbers.sh
@@ -0,0 +1,8 @@
+# execute: ./scripts/reset_numbers.sh
+# SPDX-License-Identifier: CC0-1.0 OR 0BSD
+set -x
+readonly POSIXLY_CORRECT
+export POSIXLY_CORRECT
+
+find . -type f -name info.json \
+       -execdir sh -xc 'jq --raw-output .number ./info.json > ./number.txt'
