4fa685bc60b4bf05a61573af3300373a6f2a41fc
Gabriel “gabldotink” <gabl@gabl.ink>
2024-05-16 11:07:22 -0600

    remove reset_infojson.sh and reset_num.sh
    these would’ve been better as part of commit messages in the first place

--- scripts/reset_num.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# ./scripts/reset_numbers.sh
-# SPDX-License-Identifier: CC0-1.0 OR 0BSD
-set -x
-readonly POSIXLY_CORRECT
-export POSIXLY_CORRECT
-
-find . -type f -name info.json \
-       -execdir sh -xc 'jq --raw-output .num ./info.json > ./num.txt' ';'
