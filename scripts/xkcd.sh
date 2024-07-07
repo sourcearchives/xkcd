df87b4e3b7b462d0e3e21859168b06e49e7756d1
Gabriel “gabldotink” <gabl@gabl.ink>
2024-07-07 17:10:53 -0600

    make xkcd.sh consistent with xk3d.sh

--- scripts/xkcd.sh
+++ scripts/xkcd.sh
@@ -8,7 +8,9 @@ arg="$1"
 readonly arg
 export arg
 
+# make sure that the argument is an integer (“number”)
 if [ "$(printf '%s' "$arg"|grep -e '^[0-9]*$')" = '' ];then
+  # the argument is not an integer (e.g. empty, “-h,” “--help,” “-?,” etc.), so print usage
   printf \
 'usage: ./scripts/xkcd.sh <0/00/000/0000>
 Please run this script from the repository root.
@@ -18,20 +20,24 @@ It automatically handles 1x and 2x images; info.json; title.txt, alt.txt,
 transcript.txt, link.txt, and news.html.
 It currently doesn’t do anything past that. You might want to check the
 comics you’re downloading first for potential issues.\n'
+  # exit unsuccessfully
   exit 1
 fi
 
+# remove leading “0s” from the number by multiplying by 1
 num="$(($arg*1))"
 readonly num
 export num
 
-if ! curl -fI "https://xkcd.com/$num/";then
+# make sure that the number doesn’t give an error
+if ! curl -Ifs "https://xkcd.com/$num/";then
   printf \
 'Couldn’t find “xkcd” %s online.
 Make sure it exists and that you’re connected to the Internet.\n' "$num"
   exit 1
 fi
 
+# add padding with “0s” to number, for compatibility with directory structure
 if   [ "${#num}" = 1 ];then
   pad=000
 elif [ "${#num}" = 2 ];then
@@ -41,20 +47,24 @@ elif [ "${#num}" = 3 ];then
 elif [ "${#num}" = 4 ];then
   pad=''
 else
+  # given “xkcd”’s schedule stays consistent, this code should work until July 8, 2069
   printf \
 'The number is more than 4 digits long.
 Please file an issue at
 https://github.com/sourcearchives/xkcd
 if this is a valid comic number.'
+  # exit unsuccessfully
   exit 1
 fi
 readonly pad
 export pad
 
+# extract first two digits of number for categorization
 hundred="$(printf '%s\n' "$pad$num"|cut -c1-2)"
 readonly hundred
 export hundred
 
+# the first 99 comics are categorized differently
 if [ "$hundred" = 00 ];then
   dir="./content/en/xkcd/comics/0001-0099/$pad$num"
 else
@@ -68,9 +78,11 @@ if ! mkdir -p "$dir";then
   exit 1
 fi
 
+# download JSON and minify it
 curl -o - "https://xkcd.com/$num/info.0.json" | \
 jq --compact-output --monochrome-output . - > "$dir/info.json"
 
+# extract some JSON fields to files
 jq --raw-output --monochrome-output .alt "$dir/info.json" > "$dir/alt.txt" &
 jq --raw-output --monochrome-output .link "$dir/info.json" > "$dir/link.txt" &
 jq --raw-output --monochrome-output .news "$dir/info.json" > "$dir/news.html" &
@@ -83,14 +95,17 @@ img="$(jq --raw-output --monochrome-output .img "$dir/info.json")"
 readonly img
 export img
 
+# extract the file extension
 ext="${img##*.}"
 readonly ext
 export ext
 
-base="$(printf '%s' "$img"|sed 's/\.'"$ext"'//g')"
+# extract the filename without the extension
+base="$(printf '%s' "$img" | sed 's/\.'"$ext"'//g')"
 readonly base
 export base
 
+# download image, plus 2x image if it exists
 curl -o "$dir/1x.$ext" "$img" &
 curl -fo "$dir/2x.$ext" "${base}_2x.$ext" &
 wait
@@ -99,20 +114,19 @@ lf="$(printf '\n')"
 readonly lf
 export lf
 
-if [ "$(cat "$dir/link.txt")" = "$lf" ];then
+# if these files are blank, then delete them
+[ "$(cat "$dir/link.txt")" = "$lf" ] &&
   rm -f "$dir/link.txt"
-fi
 
-if [ "$(cat "$dir/news.html")" = "$lf" ];then
+[ "$(cat "$dir/news.html")" = "$lf" ] &&
   rm -f "$dir/news.html"
-fi
 
-if [ "$(cat "$dir/transcript.txt")" = "$lf" ];then
+[ "$(cat "$dir/transcript.txt")" = "$lf" ] &&
   rm -f "$dir/transcript.txt"
-fi
 
 printf \
 'Done.
 %s\n' "$dir"
 
+# exit successfully
 exit 0
